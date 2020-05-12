f74e5bb4688f14e536523c623452156ecf7ffbd0
Kim Suhyeon <s2018s26@e-mirim.hs.kr>
2020-05-12 12:44:36 +0900

    아따..이해를못하겠서라 밥먹고 돌아오겠음

--- antsequence20th/Program.cs
+++ antsequence20th/Program.cs
@@ -10,6 +10,30 @@ namespace antsequence20th
     {
         static void Main(string[] args)
         {
+            string num="1";
+            int i;
+
+            for (i = 0; i < 20; i++)
+            {
+                Console.Write((i + 1) + "번째 수열 : "+num);
+                int cnt = 0;
+                char check = num[0];
+            
+                for(int j = 0; j < num.Length; i++)
+                {
+                    if (check == num[j])
+                    {
+                        cnt++;
+                    }
+                    else
+                    {
+                        
+                    }
+                }
+                
+            }
+
+
         }
     }
 }
